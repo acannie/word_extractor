5ea65ff4b1b1475279df4b56f0cd1be0042eab1d
acannie <acannie0530@gmail.com>
2021-01-12 03:51:37 +0900

    code: refactored into a class.

--- word_extractor.py
+++ word_extractor.py
@@ -1,53 +1,77 @@
 import glob
 
-def should_add_alphabet_to_word(c="", word=""):
-    if c.islower(): # 小文字のとき
-        return True
-    elif len(word) == 1: # 大文字だが 1 文字目のとき
-        return True
-    elif word.isupper(): # 単語すべてが大文字のとき
-        return True
-    return False
+class WordExtractor:
+    # 単語の辞書 (最終的に返す)
+    dictionary_set = set()
 
-def delete_short_word(dictionary_set=set()):
-    new_dictionary_set = set(dictionary_set)
-    for word in dictionary_set:
-        if len(word) < 2:
-            new_dictionary_set.remove(word)
-    return new_dictionary_set
+    # 単語を抽出するファイルの一覧
+    FILE_LIST = glob.glob("./src/*")
 
-def make_lower_in_dictionary_set(dictionary_set=set()):
-    for word in dictionary_set:
-        word = word.lower()
-    return dictionary_set
+    # constructor
+    def __init__(self):
+        pass
 
+    # 与えられた文字を 1 単語に追加すべきか判定
+    def __should_add_alphabet_to_word(self, c="", word=""):
+        if c.islower(): # 小文字のとき
+            return True
+        elif len(word) == 1: # 大文字だが 1 文字目のとき
+            return True
+        elif word.isupper(): # 単語すべてが大文字のとき
+            return True
+        return False
 
-# 単語の辞書
-dictionary_set = set()
+    # 0 または 1 文字の単語を削除
+    def __delete_short_word(self, dictionary_set=set()):
+        new_dictionary_set = set(dictionary_set)
+        for word in dictionary_set:
+            if len(word) < 2:
+                new_dictionary_set.remove(word)
+        return new_dictionary_set
 
+    # set 中の単語をすべて小文字に変換
+    def __make_lower_in_dictionary_set(self, dictionary_set=set()):
+        new_dictionary_set = set()
+        for word in dictionary_set:
+            new_dictionary_set.add(word.lower())
+        return new_dictionary_set
 
-files = glob.glob("./src/*")
-for file in files:
-    f = open(file)
-    line_list = f.readlines()
-
-    word = ""
-
-    for line in line_list:
+    # 1 行から単語を抽出し単語の辞書に格納
+    def __make_word(self, line):
+        word = ""
         for c in line:
             if c.isalpha():
-                if should_add_alphabet_to_word(c, word):
+                if self.__should_add_alphabet_to_word(c, word): # 単語が未完成
                     word += c
-                else:
-                    dictionary_set.add(word)
+                else: # 単語が完成した (キャメルケース等アルファベットが連続している場合)
+                    self.dictionary_set.add(word)
                     word = ""
                     word += c
-            else:
-                dictionary_set.add(word)
+            else: # 単語が完成した (スネークケース等)
+                self.dictionary_set.add(word)
                 word = ""
-
-    f.close()
     
-dictionary_set = delete_short_word(dictionary_set)
-dictionary_set = make_lower_in_dictionary_set(dictionary_set)
-print(dictionary_set)
+    # 単語の辞書を作る
+    def __make_word_dictionary(self, line_list):
+        for line in line_list:
+            self.__make_word(line)
+
+    # 単語の辞書に後処理を行う
+    def __final_process(self):
+        self.dictionary_set = self.__delete_short_word(self.dictionary_set)
+        self.dictionary_set = self.__make_lower_in_dictionary_set(self.dictionary_set)
+
+    # メインの処理
+    def word_extract(self):
+        for file in self.FILE_LIST:
+            f = open(file)
+            line_list = f.readlines()
+            self.__make_word_dictionary(line_list)
+            f.close()
+
+        self.__final_process()
+        return self.dictionary_set
+
+if __name__ == "__main__":
+    word_extractor = WordExtractor()
+    print(word_extractor.word_extract())
