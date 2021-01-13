a7184bc7f11ef23461586e18fe443dcfd75aac85
acannie <acannie0530@gmail.com>
2021-01-14 04:06:40 +0900

    shell: added sample to run.

---
+++ run_sample.sh
@@ -0,0 +1,8 @@
+# for file
+pipenv run python word_extractor.py sample.c c
+# pipenv run python output_to_excel.py sample.c c refernce.xlsx output.xlsx
+
+# for folder
+pipenv run python word_extractor.py sample/ c
+pipenv run python output_to_excel.py sample/ c reference.xlsx output.xlsx
+ 
