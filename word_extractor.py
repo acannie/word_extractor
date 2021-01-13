69fef5abe4fefbd3e7df4162eada89914119097d
acannie <acannie0530@gmail.com>
2021-01-13 21:45:49 +0900

    code: updated word_extractor.

--- word_extractor.py
+++ word_extractor.py
@@ -8,7 +8,7 @@ class WordExtractor:
 
     # 予約語一覧
     RESERVED_WORD_C = ["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if",
-                       "int", "long", "register", "return", "signed", "sizeof", "short", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
+                       "int", "long", "register", "return", "signed", "sizeof", "short", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "define", "include", "main"]
     RESERVED_WORD_CPP = RESERVED_WORD_C + ["asm", "catch", "class", "delete", "friend", "inline",
                                            "new", "operator", "overload", "private", "protected", "public", "template", "this", "throw", "try", "virtual"]
     RESERVED_WORD_PYTHON = keyword.kwlist
@@ -47,47 +47,54 @@ class WordExtractor:
             new_dictionary_set.add(word.lower())
         return new_dictionary_set
 
-    # 与えられた文字を 1 単語に追加すべきか判定
+    # 与えられた文字は新しい単語の1文字目か判定
 
-    def __should_add_alphabet_to_word(self, c="", word=""):
+    def __is_new_word(self, c="", word=""):
         if c.islower():  # 小文字のとき
             if word.isupper() and len(word) > 2:  # 単語すべてが大文字かつ 2 文字以上のとき
-                return False
-            return True
+                return True
+            return False
         else:
             if word.islower():  # 大文字だが 1 文字目のとき
-                return False
-            if word.isupper():  # 単語すべてが大文字のとき
                 return True
-        return False
+            if word.isupper():  # 単語すべてが大文字のとき
+                return False
+        return True
 
-    # 1 行から単語を抽出し単語の辞書に格納
-
-    def __make_word(self, spaceSeparated_word_list=[]):
-        for spaceSeparated_word in spaceSeparated_word_list:
-            if not(spaceSeparated_word in self.RESERVED_WORD):  # 予約語は含めない (前後空白のときのみ有効)
-                word = ""
-                for c in spaceSeparated_word:
-                    if c.isalpha():
-                        # 単語が未完成
-                        if self.__should_add_alphabet_to_word(c, word):
-                            word += c
-                        else:  # 単語が完成した (キャメルケース等アルファベットが連続している場合)
-                            WordExtractor.dictionary_set.add(word)
-                            word = ""
-                            word += c
-                    else:  # 単語が完成した (スネークケース等)
-                        WordExtractor.dictionary_set.add(word)
-                        word = ""
-                # spaceSeparated_word を走査し終えた時点で word の中身があれば返す
+    # 1 ブロックから単語を抽出し単語の辞書に格納
+    def __make_word(self, spaceSeparated_word=""):
+        if spaceSeparated_word in self.RESERVED_WORD:  # 予約語は含めない (前後空白のときのみ有効)
+            return
+        word = ""
+        for c in spaceSeparated_word:
+            if c.isalpha():
+                if self.__is_new_word(c, word):  # 単語が完成した (文字を次の単語に含める。キャメルケース)
+                    WordExtractor.dictionary_set.add(word)
+                    word = ""
+                word += c
+            else:  # 単語が完成した (文字を次の単語に含めない。スネークケース)
                 WordExtractor.dictionary_set.add(word)
+                word = ""
+        # spaceSeparated_word を走査し終えた時点で word の中身があれば単語の辞書に加える
+        WordExtractor.dictionary_set.add(word)
+
+    # 余計な記号を半角スペースに置き換える
+    def __delete_unusable_symbols(self, line="", usable_symbols=["_"]):
+        new_line = line
+        for i in range(len(line)):
+            if (not line[i].isalpha()) and (not line[i] in usable_symbols):  # 命名に使えない記号
+                new_line = new_line[:i] + ' ' + new_line[i+1:]
+        return new_line
 
     # 単語の辞書を作る
-
-    def __make_word_dictionary(self, line_list=[]):
+    def __make_word_dictionary(self, line_list=[""]):
         for line in line_list:
+            line = self.__delete_unusable_symbols(
+                line=line, usable_symbols=["_"])
             spaceSeparated_word_list = line.split(" ")
-            self.__make_word(spaceSeparated_word_list)
+            spaceSeparated_word_list.remove("")
+            for spaceSeparated_word in spaceSeparated_word_list:
+                self.__make_word(spaceSeparated_word)
 
     # 単語の辞書に後処理を行う
     def __final_process(self):
@@ -102,7 +109,7 @@ class WordExtractor:
         for file in self.FILE_LIST:
             f = open(file)
             line_list = f.readlines()
-            self.__make_word_dictionary(line_list)
+            self.__make_word_dictionary(line_list=line_list)
             f.close()
 
         self.__final_process()
@@ -111,7 +118,5 @@ class WordExtractor:
 
 if __name__ == "__main__":
     # choose from: c, c++, python
-    word_extractor = WordExtractor(src_folder="./src/", language="python")
+    word_extractor = WordExtractor(src_folder="./src/", language="c")
     print(word_extractor.word_extract())
-
-a = {"word": {"num": 2, "file": "file_name"}}
